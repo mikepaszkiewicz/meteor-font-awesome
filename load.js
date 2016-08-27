dcfd988dc65c5dd90e0e271c0c6b2eb856104772
MICHAEL PASZKIEWICZ <michaelpaszkiewicz@MICHAELs-MacBook-Pro.local>
2016-08-27 16:27:54 -0400

    init

--- load.js
+++ load.js
@@ -4,7 +4,7 @@ var head = document.getElementsByTagName('head')[0];
 var style = document.createElement('link');
 style.type = 'text/css';
 style.rel = "stylesheet";
-style.href = '//maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css';
+style.href = '//maxcdn.bootstrapcdn.com/font-awesome/4.6.0/css/font-awesome.min.css';
 
 //Generate fa-animation tag
 var anim = document.createElement('link');
